425fe85588754a7d423e9bfd0a66765d9d1ec043
Dmitry Zvorykin <dmitry.zvorikin@gmail.com>
2017-06-20 20:04:29 +0300

    Initial Commit

---
+++ 20170620-adapter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  20170620-adapter
+//
+//  Created by iOS-School-1 on 20/06/2017.
+//  Copyright © 2017 iOS-School-1. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
